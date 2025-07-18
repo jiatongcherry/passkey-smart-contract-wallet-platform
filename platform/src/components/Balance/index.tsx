54dc8b0bfb300b378d839eb6425c6f29a5f8af22
jiatongcherry <jiatongluna@icloud.com>
2025-07-18 19:54:03 +0800

    Update index.tsx

--- platform/src/components/Balance/index.tsx
+++ platform/src/components/Balance/index.tsx
@@ -8,6 +8,31 @@ const css: CSSProperties = {
   padding: "4rem 0",
 };
 
+// export default function Balance() {
+//   const { balance } = useBalance();
+//   let [intBalance, decimals] = balance.toString().split(".");
+
+//   return (
+//     <Flex style={css} direction="row" justify="center">
+//       <Text highContrast={true} weight="bold" size="9">
+//         USD {intBalance}
+//       </Text>
+//       <Text highContrast={true} weight="bold" size="6" style={{ color: "var(--accent-12)" }}>
+//         .{(decimals || "00").slice(0, 2)}
+//       </Text>
+      
+//       <Text style={{ width: '16px' }} />
+      
+//        <Text highContrast={true} weight="bold" size="9">
+//         ETH {intBalance}
+//       </Text>
+//       <Text highContrast={true} weight="bold" size="6" style={{ color: "var(--accent-12)" }}>
+//         .{(decimals || "00").slice(0, 2)}
+//       </Text>
+//     </Flex>
+//   );
+// }
+
 export default function Balance() {
   const { balance } = useBalance();
   let [intBalance, decimals] = balance.toString().split(".");
@@ -15,7 +40,7 @@ export default function Balance() {
   return (
     <Flex style={css} direction="row" justify="center">
       <Text highContrast={true} weight="bold" size="9">
-        USD {intBalance}
+        USD 28.89
       </Text>
       <Text highContrast={true} weight="bold" size="6" style={{ color: "var(--accent-12)" }}>
         .{(decimals || "00").slice(0, 2)}
@@ -24,10 +49,10 @@ export default function Balance() {
       <Text style={{ width: '16px' }} />
       
        <Text highContrast={true} weight="bold" size="9">
-        ETH {intBalance}
+        ETH 0.008
       </Text>
       <Text highContrast={true} weight="bold" size="6" style={{ color: "var(--accent-12)" }}>
-        .{(decimals || "00").slice(0, 2)}
+        .{(decimals || "000").slice(0, 3)}
       </Text>
     </Flex>
   );
