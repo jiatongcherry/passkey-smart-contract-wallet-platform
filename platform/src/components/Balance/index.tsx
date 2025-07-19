c50ea24d140271da369cf5e3ffe9d82d4b6c736a
jiatongcherry <jiatongluna@icloud.com>
2025-07-19 21:54:39 +0800

    Update index.tsx

--- platform/src/components/Balance/index.tsx
+++ platform/src/components/Balance/index.tsx
@@ -8,31 +8,6 @@ const css: CSSProperties = {
   padding: "4rem 0",
 };
 
-// export default function Balance() {
-//   const { balance } = useBalance();
-//   let [intBalance, decimals] = balance.toString().split(".");
-
-//   return (
-//     <Flex style={css} direction="row" justify="center">
-//       <Text highContrast={true} weight="bold" size="9">
-//         USD {intBalance}
-//       </Text>
-//       <Text highContrast={true} weight="bold" size="6" style={{ color: "var(--accent-12)" }}>
-//         .{(decimals || "00").slice(0, 2)}
-//       </Text>
-      
-//       <Text style={{ width: '16px' }} />
-      
-//        <Text highContrast={true} weight="bold" size="9">
-//         ETH {intBalance}
-//       </Text>
-//       <Text highContrast={true} weight="bold" size="6" style={{ color: "var(--accent-12)" }}>
-//         .{(decimals || "00").slice(0, 2)}
-//       </Text>
-//     </Flex>
-//   );
-// }
-
 export default function Balance() {
   const { balance } = useBalance();
   let [intBalance, decimals] = balance.toString().split(".");
@@ -40,20 +15,45 @@ export default function Balance() {
   return (
     <Flex style={css} direction="row" justify="center">
       <Text highContrast={true} weight="bold" size="9">
-        USD 28
+        USD {intBalance}
       </Text>
       <Text highContrast={true} weight="bold" size="6" style={{ color: "var(--accent-12)" }}>
-        .{("89" || "00").slice(0, 2)}
+        .{(decimals || "00").slice(0, 2)}
       </Text>
       
       <Text style={{ width: '16px' }} />
       
        <Text highContrast={true} weight="bold" size="9">
-        ETH 0
+        ETH {intBalance}
       </Text>
       <Text highContrast={true} weight="bold" size="6" style={{ color: "var(--accent-12)" }}>
-        .{("008" || "000").slice(0, 3)}
+        .{(decimals || "00").slice(0, 2)}
       </Text>
     </Flex>
   );
 }
+
+// export default function Balance() {
+//   const { balance } = useBalance();
+//   let [intBalance, decimals] = balance.toString().split(".");
+
+//   return (
+//     <Flex style={css} direction="row" justify="center">
+//       <Text highContrast={true} weight="bold" size="9">
+//         USD 28
+//       </Text>
+//       <Text highContrast={true} weight="bold" size="6" style={{ color: "var(--accent-12)" }}>
+//         .{("89" || "00").slice(0, 2)}
+//       </Text>
+      
+//       <Text style={{ width: '16px' }} />
+      
+//        <Text highContrast={true} weight="bold" size="9">
+//         ETH 0
+//       </Text>
+//       <Text highContrast={true} weight="bold" size="6" style={{ color: "var(--accent-12)" }}>
+//         .{("008" || "000").slice(0, 3)}
+//       </Text>
+//     </Flex>
+//   );
+// }
